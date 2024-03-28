e5e1df02f6a319b5a14e19f6bbab6e009f1d9f9c
Fabricio Esmerio <fabricio1esmerio@gmail.com>
2024-03-28 00:37:11 -0300

    feat: adiciona referência da lib no projeto principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ConfigFeEditableRowTable } from 'fe-editable-row-table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'editable-row-table';
+  data: any[] = [];
+  config: ConfigFeEditableRowTable = {
+    primarykey: "id",
+    tableConfig: [
+      {
+        header: 'Header 1',
+        model: 'model1',
+        editable: false,
+        type: 'text'
+      },
+      {
+        header: 'Header 2',
+        model: 'model2',
+        editable: false,
+        type: 'text'
+      },
+      {
+        header: 'Header 3',
+        model: 'model3',
+        editable: false,
+        type: 'text'
+      },
+      {
+        header: 'Header 4',
+        model: 'model4',
+        editable: false,
+        type: 'text'
+      },
+      {
+        header: 'Header 5',
+        model: 'model5',
+        editable: false,
+        type: 'text'
+      }
+    ],
+    tableTitle: 'Exemplo legal',
+    bntActions: [
+      {
+        label: "Ação 1",
+        event: "event",
+        type: 'primary'
+      },
+      {
+        label: "Ação 2",
+        event: "event2",
+        type: 'danger'
+      },
+      {
+        label: "Ação 3",
+        event: "event3",
+        type: 'warning'
+      }
+    ]
+  };
+
+  ngOnInit(): void {
+    for (let i = 0; i < 100; i++) {
+      this.data.push({
+        model1: Math.random(),
+        model2: Math.random() * 10,
+        model3: Math.random() * 100,
+        model4: Math.random() * 1000,
+        model5: Math.random() * 1200
+      })
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FeEditableRowTableModule } from 'fe-editable-row-table';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FeEditableRowTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
